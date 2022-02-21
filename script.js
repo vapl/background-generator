2a3c047ae5c2d91082a6a15907bd33a710bcdfbd
vapl <valdis.vascenkovs@gmail.com>
2022-02-21 22:40:50 +0200

    adding CopyToClipboard button

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const colorPiker1 = document.querySelector("#color-1");
 const colorPiker2 = document.querySelector("#color-2");
 const h3 = document.querySelector("h3");
 const background = document.querySelector("body");
+const iCopy = document.querySelector("i");
+const spanTooltip = document.querySelector("span");
 
 // background default values
 background.style.setProperty("--color-1", colorPiker1.value);
@@ -17,5 +19,17 @@ addEventListener("input", (event) => {
     let color1 = getComputedStyle(background).getPropertyValue("--color-1");
     let color2 = getComputedStyle(background).getPropertyValue("--color-2");
     h3.innerText = `linear-gradient(to right, ${color1}, ${color2});`
-})
 
+    if (h3.innerText.includes("linear")) {
+        iCopy.classList.add("visible");
+    }
+});
+
+// Copy to clipboard
+const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text);
+}
+
+iCopy.addEventListener("click", () => {
+    copyToClipboard(h3.innerText);
+});
